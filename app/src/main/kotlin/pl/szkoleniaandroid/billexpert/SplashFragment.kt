a909f132c163a441cdf40e6912c9277c3e2c09ae
mic1410 <mic1410@gmail.com>
2019-10-23 10:56:45 +0200

    use LiveData to observe and show Toast when error

--- app/src/main/kotlin/pl/szkoleniaandroid/billexpert/SplashFragment.kt
+++ app/src/main/kotlin/pl/szkoleniaandroid/billexpert/SplashFragment.kt
@@ -5,10 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 
 class SplashFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.splash_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val navController = findNavController()
+        navController.navigate(SplashFragmentDirections.navSplashNotSignedIn())
     }
 }
--- app/src/main/kotlin/pl/szkoleniaandroid/billexpert/features/signin/SignInFragment.kt
+++ app/src/main/kotlin/pl/szkoleniaandroid/billexpert/features/signin/SignInFragment.kt
@@ -4,8 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.ObservableBoolean
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import pl.szkoleniaandroid.billexpert.R
 import pl.szkoleniaandroid.billexpert.databinding.ActivityLoginBinding
 import pl.szkoleniaandroid.billexpert.domain.usecase.SignInUseCase
@@ -14,16 +19,27 @@ import pl.szkoleniaandroid.billexpert.utils.ObservableString
 
 class SignInFragment : Fragment() {
 
+    lateinit var viewModel: SignInViewModel
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         //data binding, will make login screen show
         // return ActivityLoginBinding.inflate(inflater, container, false).root
 
         return ActivityLoginBinding.inflate(inflater, container, false).apply {
-            this.vm = SignInViewModel(
+            viewModel = SignInViewModel(
                     stringProvider = ContextStringProvider(requireContext()),
                     signInUseCase = SignInUseCase()
             )
+            this.vm = viewModel
         }.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //me: this will be automatically unregistered when fragment (this) is destroyed
+        viewModel.showErrorLiveData.observe(this, Observer {
+            Toast.makeText(requireContext(), it, Toast.LENGTH_SHORT).show()
+        })
     }
 
 }
@@ -39,9 +55,12 @@ class SignInViewModel(
     val passwordError = ObservableString("")
     val inProgress = ObservableBoolean(false)
 
+    val showErrorLiveData = MutableLiveData<String>("")
+
     fun loginClicked() {
         var isValid = true
         if (username.get().isNullOrEmpty()) {
+            //me: in pure Clean Architecture String should also be provided by e.g. new StringErrorProvider class
             usernameError.set(stringProvider.getString(R.string.username_cant_be_empty))
             isValid = false
         }
@@ -56,6 +75,8 @@ class SignInViewModel(
             if (result) {
                 //GO TO BILLS
             } else {
+                //me: in pure Clean Architecture String should also be provided by e.g. new StringErrorProvider class
+                showErrorLiveData.value = stringProvider.getString(R.string.invalid_credentials)
                 //SHOW ERROR
             }
 
